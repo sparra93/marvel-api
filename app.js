5d0c88bc3c562a510f7673fdae09df51782c2b39
Sergio Parra Santiago <soul93@MacBook-Pro-de-macbook.local>
2019-08-16 23:04:39 -0400

    Update cors

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 const routes = require('./app/routes');
 
 const app = express();
+app.use(bodyParser.json());
+routes(app);
 app.use(cors({
     origin: 'https://angular-marvel-app.herokuapp.com',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }));
-app.use(bodyParser.json());
-routes(app);
 
 app.listen(process.env.PORT || 9000, () => {
     console.log('Started on port ', process.env.PORT || 9000);
